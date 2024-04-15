4f41947895c5e527de90fe5e8836e10e6de9d4be
Hugo Santos <89364200+llZektorll@users.noreply.github.com>
2024-04-15 20:32:59 +0100

    Create 034 - DeleteInvalidAccounts.ps1

---
+++ 03 - ExchangeOnline/034 - DeleteInvalidAccounts.ps1
@@ -0,0 +1,101 @@
+<# 
+.DESCRIPTION 
+    Full discription
+.NOTES 
+    Vertsion:   1.0
+    Author: Hugo Santos (https://github.com/llZektorll)
+    Creation Date: 2024-04-15 (YYYY-MM-DD)
+.LINK 
+    Script repository: https://github.com/llZektorll/Microsoft-PowerShell-Fastlane
+#>
+#region Variables
+$Global:ErrorActionPreference = 'Stop'
+$RootLocation = 'C:\Temp'
+$LogFile = "$($RootLocation)\Logs\Log$(Get-Date -Format 'yyyyMM').txt"
+#Account Identification
+$Account = 'contoso@contoso.com'
+$TrusteeName = 'S-1-5-*'
+#endregion 
+
+#region Functions
+#region Variable Cleaner
+Function VarCleaner {
+    $RootLocation = $null
+    $LogFile = $null
+    $Message = $null
+    $ForegroundColor = $null
+    $TrusteeName = $null
+    $Account = $null
+}
+#endregion
+#region Ensure TLS 1.2
+Function ForceTLS {
+    Try {
+        If ([Net.SecurityProtocolType]::Tls12 -bor $False) {
+            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+            Write-Log "`t Forced TLS 1.2 since its not server default"
+        } Else {
+            Write-Log "`t TLS 1.2 already configured as server default"
+        }
+    } Catch {
+        Write-Log "`t Unable to check or ensure TLS 1.2 status"
+        Write-Log "`t Error: $($_.Exception.Message)"
+    }
+}
+#endregion
+#region Check Log File Location
+Function CheckFilePath {
+    If (Test-Path -Path "$($RootLocation)\Logs\") {}Else {
+        New-Item "$($RootLocation)\Logs" -ItemType Directory
+    }
+}
+#endregion
+#region Write Log
+function Write-Log {
+    param (
+        $Message,
+        $ForegroundColor = 'White'
+    )
+    function TimeStamp { return '[{0:yyyy/MM/dd} {0:HH:mm:ss}]' -f (Get-Date) }
+
+    "$(TimeStamp) $Message" | Tee-Object -FilePath $LogFile -Append | Write-Verbose
+    Write-Host $Message -ForegroundColor $ForegroundColor
+}
+#endregion
+#endregion
+
+#region Execution
+Try {
+    CheckFilePath
+} Catch {
+    Write-Host "`t Unable to check folders for logs"
+    Write-Host "`t Error: $($_.Exception.Message)"
+}
+Write-Log "`t ==========================================="
+Write-Log "`t ==                                       =="
+Write-Log "`t ==                title                  =="
+Write-Log "`t ==                                       =="
+Write-Log "`t ==========================================="
+Write-Log "`t Start Script Run"
+Try {
+    Write-Log "`t Step 1 - Enforce TLS 1.2"
+    ForceTLS
+    
+} Catch {
+    Write-Log "`t Error: $($_.Exception.Message)"
+}
+Try {
+    Write-Log "`t Step 2 - Connecting to Exchange Online"
+    Connect-ExchangeOnline
+} Catch {
+    Write-Log "`t Error: $($_.Exception.Message)"
+}
+Try {
+    Write-Log "`t Step 3 - Removing permissions for deleted accounts"
+    Get-Mailbox -Identity $Account | ForEach-Object { Get-RecipientPermission $_.PrimarySmtpAddress } | Where-Object { $_.Trustee -like $TrusteeName } | Remove-RecipientPermission -Confirm:$false
+} Catch {
+    Write-Log "`t Error: $($_.Exception.Message)"
+}
+VarCleaner
+Write-Log "`t More scripts like this in https://github.com/llZektorll/Microsoft-PowerShell-Fastlane"
+#endregion
