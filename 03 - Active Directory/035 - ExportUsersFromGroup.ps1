3f9263a860ea87d6c97076a7fb1c47b22bbf8b97
Hugo Santos <89364200+llZektorll@users.noreply.github.com>
2025-01-07 21:39:44 +0000

    Update 2025-01-07
    Added script to get all the users from any group inside a OU

--- 03 - Active Directory/035 - ExportUsersFromGroup.ps1
+++ 03 - Active Directory/035 - ExportUsersFromGroup.ps1
@@ -1,11 +1,10 @@
-#Requires -Version 7.0
 <# 
 .DESCRIPTION 
-    Description
+    Export all users from a specific group in Active Directory
 .NOTES 
     Vertsion:   2.0
     Author: Hugo Santos (https://github.com/llZektorll)
-    Creation Date: 2025-01-01 (YYYY-MM-DD)
+    Creation Date: 2025-01-07 (YYYY-MM-DD)
 .LINK 
     Script repository: https://github.com/llZektorll/Microsoft-PowerShell-Fastlane
 #>
@@ -14,29 +13,10 @@ $Global:ErrorActionPreference = 'Stop'
 $RootLocation = 'C:\Temp\'
 $LogFile = "$($RootLocation)Logs\Log$(Get-Date -Format 'yyyyMM').txt"
 $ExportFile = "$($RootLocation)Exports\Export_$(Get-Date -Format 'yyyyMM').csv"
-#Connection
-$Tenant = 'MPFL.onmicrosoft.com'
-$TenantId = 'f1f1f1f1-f1f1-f1f1-f1f1-f1f1f1f1f1f1'
-$Application_ID = 'f1f1f1f1-f1f1-f1f1-f1f1-f1f1f1f1f1f1'
-$CertThumbprint = 'H1H1H1H1H1H1H1H1H1H1H1H1H1H1H1H1H1H1H1H1'
-$SPO_Site = 'https://MPFL-admin.sharepoint.com/'
+#Export Variables
+$AD_OU = 'OU=MyGroupLocation,OU=O365,DC=MPFL,DC=pt'
 #endregion
 #region Main Functions
-#region Ensure TLS 1.2
-Function ForceTLS {
-    Try {
-        If ([Net.SecurityProtocolType]::Tls12 -bor $False) {
-            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-            Write-Log "`t Forced TLS 1.2 since its not server default"
-        } Else {
-            Write-Log "`t TLS 1.2 already configured as server default"
-        }
-    } Catch {
-        Write-Log "`t Unable to check or ensure TLS 1.2 status"
-        Write-Log "`t Error: $($_.Exception.Message)"
-    }
-}
-#endregion
 #region Check Log File Location
 Function CheckFilePath {
     If (Test-Path -Path "$($RootLocation)\Logs\") {}Else {
@@ -69,21 +49,23 @@ Try {
 }
 Write-Log "`t ==========================================="
 Write-Log "`t ==                                       =="
-Write-Log "`t ==                title                  =="
+Write-Log "`t ==  Export Users From All Groups in OU   =="
 Write-Log "`t ==                                       =="
 Write-Log "`t ==========================================="
 Write-Log "`t Start Script Run"
 Try {
-    Write-Log "`t Step 1 - Enforce TLS 1.2"
-    ForceTLS
+    Write-Log "`t Step 1 - Export Users"
+    $Groups = Get-ADGroup -SearchBase $AD_OU -Properties * -Filter *
+    ForEach($Group in $Groups){
+        $ObjectDetails = [PSCustomObject][Ordered]@{
+            Name              = $group.name
+            Members           = $group.members -join ";"
+        }
+    }
+    $ObjectDetails | Export-Csv $ExportFile -Delimiter "," -Encoding UTF8 -NoClobber -NoTypeInformation -Append -Force
 } Catch {
     Write-Log "`t Error: $($_.Exception.Message)"
 }
-Try {
-    Write-Log "`t Step 2 - "
 
-} Catch {
-    Write-Log "`t Error: $($_.Exception.Message)"
-}
 Write-Log "`t More scripts like this in https://github.com/llZektorll/Microsoft-PowerShell-Fastlane"
 #endregion
