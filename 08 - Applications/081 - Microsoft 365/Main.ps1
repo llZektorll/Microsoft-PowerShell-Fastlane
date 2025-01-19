e41efc1dff3bd816ac677c5edd2539b5b554e71d
Hugo Santos <89364200+llZektorll@users.noreply.github.com>
2025-01-19 16:05:04 +0000

    Update Main.ps1

--- 08 - Applications/081 - Microsoft 365/Main.ps1
+++ 08 - Applications/081 - Microsoft 365/Main.ps1
@@ -2,8 +2,9 @@
 .DESCRIPTION 
     This script will create a UI for the configurations and costumization of Microsoft 365
 .NOTES 
-    Version:   1.0
-    Author: Hugo Santos (https://github.com/llZektorll)
+    Version:   1.0.1
+    Author: Hugo Santos 
+    GitHub: https://github.com/llZektorll
     Creation Date: 2025-01-19 (YYYY-MM-DD)
 #>
 
